5a3ff61d3516661943cbcaf6b4a2371be5a633fc
TrWestdoor <374251766@qq.com>
2019-06-20 17:41:15 +0800

    done dict

--- task1/LR.py
+++ task1/LR.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 
 def load_data():
@@ -16,11 +17,33 @@ def create_dic(dataMat):
     Dict = []
     for line in dataMat:
         line = line[2].split()
-        Dict += line
-    Dict = set(Dict)
+        for element in line:
+            if element not in Dict and element not in [',', '.']:
+                Dict.append(element)
     return Dict
 
 
-dataMat, labelMat = load_data()
-Dict = create_dic(dataMat)
-print(Dict)
+def create_vector(dict_, dataMat):
+    m = len(dataMat)
+    n = len(dict_)
+    dataMatrix = np.zeros((m, n))
+    for i in range(m):
+        line = dataMat[i][2].strip().split()
+        for word in line:
+            if word not in [',', '.']:
+                dataMatrix[i][dict_.index(word)] = 1
+    return dataMatrix
+
+
+if __name__ == '__main__':
+    dataMat, labelMat = load_data()
+    if os.path.exists('dict.txt'):
+        fr = open('dict.txt', 'r')
+        Dict = fr.readline().strip().split()
+    else:
+        Dict = create_dic(dataMat)
+        fw = open('dict.txt', 'w')
+        fw.write(' '.join(Dict))
+
+    dataMatrix = create_vector(Dict, dataMat)
+    print(np.shape(dataMatrix))
